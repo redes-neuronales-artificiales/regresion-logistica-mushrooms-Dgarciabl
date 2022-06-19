299bb0655b5b809431c7ee8ebd32f958b7e9e762
David Garcia Blandon <dgarciabl@unal.edu.co>
2022-06-19 16:36:20 -0500

    correcion punto 1

--- preguntas.py
+++ preguntas.py
@@ -68,7 +68,7 @@ def pregunta_01():
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    df.drop('veil-type', axix = 1, inplace = True)
+    df.drop('veil-type', axis = 1, inplace = True)
 
     # Asigne la columna `type` a la variable `y`.
     y = df['type']
@@ -77,7 +77,7 @@ def pregunta_01():
     X = df.copy()
 
     # Remueva la columna `type` del DataFrame `X`.
-    X.drop('type', axix = 1, inplace = True)
+    X.drop('type', axis = 1, inplace = True)
 
     # Retorne `X` y `y`
     return X, y
